f589928344262a964d732469fce3da2a39b849f4
shadab012s <mdshadab0012s@gmail.com>
2025-01-15 22:09:33 +0530

    adding https in search api

--- src/utils/constantss.js
+++ src/utils/constantss.js
@@ -4,6 +4,6 @@
 export const OFFSET_LIVE_CHAT=10;
 
 export const YOUTUBE_VIDEO_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=50&chart=mostPopular&regionCode=IN&key="+process.env.REACT_APP_API_KEY;
-export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API="https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 export const YOUTUBE_SEARCH_API_VALUE="https://youtube.googleapis.com/youtube/v3/search?part=snippet&safeSearch=strict&type=videos&videoCaption=any&videoType=any&key="+process.env.REACT_APP_API_KEY;
 
